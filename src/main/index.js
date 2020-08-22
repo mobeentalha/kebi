1c9fd195bff970b660f3795f95adce8bec1c0694
qasimkhan1323 <qasim.khan@funavry.com>
2020-08-22 18:08:29 +0500

    changes assets folder location

--- src/main/index.js
+++ src/main/index.js
@@ -1,87 +1,93 @@
-import React , {Component} from 'react';
-import { StyleSheet, Text, View, ImageBackground, TouchableOpacity } from 'react-native';
-import {Icon} from 'native-base'
-import {Back} from '../asstes/images'
-import SideBar from '../sidebar'
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  TouchableOpacity,
+} from "react-native";
+import { Icon } from "native-base";
+import { Back } from "../../asstes/images";
+import SideBar from "../sidebar";
 class Main extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        initialLoading: true,
-      };
-    }
-    render() {
-        return (
-                <View style={styles.container}>
-                    <ImageBackground source={Back} style={styles.image}>
-                        <View style={styles.bar}>
-                            <SideBar />
-                        </View>
-                        <View style={styles.form}>
-                            <View style={styles.row}>
-                                <View style={styles.column}>
-                                    <Icon type="FontAwesome" name='home' style={styles.icon} />
-                                    <Text style={styles.iconText}> Laundary </Text>
-                                </View>
-                                <View style={styles.column}>
-                                    <Icon type="FontAwesome" name='home' style={styles.icon} />
-                                    <Text style={styles.iconText}> Lawn Man </Text>
-                                </View>
-                                <View style={styles.column}>
-                                    <Icon type="FontAwesome" name='home' style={styles.icon} />
-                                    <Text style={styles.iconText}> Maid </Text>
-                                </View>
-                            </View>
-                        </View>
-                    </ImageBackground>
-                </View>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      initialLoading: true,
+    };
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <ImageBackground source={Back} style={styles.image}>
+          <View style={styles.bar}>
+            <SideBar />
+          </View>
+          <View style={styles.form}>
+            <View style={styles.row}>
+              <View style={styles.column}>
+                <Icon type="FontAwesome" name="home" style={styles.icon} />
+                <Text style={styles.iconText}> Laundary </Text>
+              </View>
+              <View style={styles.column}>
+                <Icon type="FontAwesome" name="home" style={styles.icon} />
+                <Text style={styles.iconText}> Lawn Man </Text>
+              </View>
+              <View style={styles.column}>
+                <Icon type="FontAwesome" name="home" style={styles.icon} />
+                <Text style={styles.iconText}> Maid </Text>
+              </View>
+            </View>
+          </View>
+        </ImageBackground>
+      </View>
+    );
+  }
 }
-export default Main
+export default Main;
 const styles = StyleSheet.create({
   container: {
-        flex: 1,
-        backgroundColor: 'white',
+    flex: 1,
+    backgroundColor: "white",
   },
   bar: {
-        marginTop: '20%'
+    marginTop: "20%",
   },
   image: {
-        width: '100%',
-        height: '100%'
+    width: "100%",
+    height: "100%",
   },
   HeadingText: {
-        color: '#4f93e6',
-        fontSize: 50,
-        fontWeight: 'bold',
-        textAlign: 'center'
+    color: "#4f93e6",
+    fontSize: 50,
+    fontWeight: "bold",
+    textAlign: "center",
   },
   text: {
-    color: '#4f93e6',
+    color: "#4f93e6",
     fontSize: 15,
-    fontWeight: 'bold',
-    textAlign: 'center'
-    },
+    fontWeight: "bold",
+    textAlign: "center",
+  },
   form: {
-        width: '90%',
-        height: '70%',
-        marginLeft: '5%',
-        marginTop: '10%',
+    width: "90%",
+    height: "70%",
+    marginLeft: "5%",
+    marginTop: "10%",
   },
   row: {
-      flexDirection: 'row'
+    flexDirection: "row",
   },
   column: {
-      flexDirection: 'column'
+    flexDirection: "column",
   },
   icon: {
-      color: '#1be4da',
-      fontSize: 80,
-      textAlign: 'center'
+    color: "#1be4da",
+    fontSize: 80,
+    textAlign: "center",
   },
   iconText: {
-    color: '#1be4da',
-    fontSize: 30
-  }
+    color: "#1be4da",
+    fontSize: 30,
+  },
 });
